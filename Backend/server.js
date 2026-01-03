571512c6ca93df84be8e5efdac5008f405ba9921
Rupesh kushwaha <rupeshkushwaha03032@gmail.com>
2026-01-03 07:08:29 +0530

    /add

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,7 @@ connectDB();
 app.use(express.static(path.join(__dirname, "client/dist")));
 
 // For SPA routing (React/Vite)
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/dist", "index.html"));
 });
 
