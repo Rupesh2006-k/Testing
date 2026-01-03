2c8b2211221d4984f6e220e131050b2e9c14c61b
Rupesh kushwaha <rupeshkushwaha03032@gmail.com>
2026-01-03 06:55:53 +0530

    fix __dirname commonjs issue

--- Backend/server.js
+++ Backend/server.js
@@ -1,20 +1,19 @@
-require("dotenv").config(); 
-let path = require('path')
+require("dotenv").config();
+const path = require("path");
 
 const app = require("./src/app");
 const connectDB = require("./src/db/db");
+
 const PORT = process.env.PORT || 3000;
-let __dirname = path.resolve()
+
 connectDB();
 
-
+// CommonJS me __dirname already hota hai
 app.use(express.static(path.join(__dirname, "client/dist")));
 
-app.get("*", (_, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/dist", "index.html"));
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
